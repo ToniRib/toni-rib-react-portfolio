fd25956ea38ebe7dc45f56acc64c2c8cc9e9454f
ToniRib <tonimarierib@gmail.com>
2016-10-02 12:41:54 -0600

    Add a skills file that only has an object full of skills

---
+++ src/js/skills.js
@@ -0,0 +1,32 @@
+const skills = [
+  {
+    name: 'Ruby 2.x',
+    id: 1
+  },
+  {
+    name: 'Rails 4.x',
+    id: 2
+  },
+  {
+    name: 'HTML5',
+    id: 3
+  },
+  {
+    name: 'CSS3',
+    id: 4
+  },
+  {
+    name: 'JavaScript',
+    id: 5
+  },
+  {
+    name: 'ReactJS',
+    id: 6
+  },
+  {
+    name: 'jQuery',
+    id: 7
+  }
+];
+
+module.exports = skills;
