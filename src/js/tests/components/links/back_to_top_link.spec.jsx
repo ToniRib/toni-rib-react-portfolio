01892a0317c04c11851ed8adb74b46d539641017
Toni Rib <tonimarierib@gmail.com>
2016-10-02 07:00:42 -0600

    Test the scroll functionality using the following stack overflow answer:
    http://stackoverflow.com/questions/39809046/test-custom-method-on-react-component-has-been-called-using-enzyme-and-sinon
    
    This seems pretty roundabout; will likely want to retry testing with spies.

--- src/js/tests/components/links/back_to_top_link.spec.jsx
+++ src/js/tests/components/links/back_to_top_link.spec.jsx
@@ -7,5 +7,16 @@ describe('<BackToTopLink/>', () => {
     expect(wrapper).to.have.tagName('div');
   });
 
-//  TODO: Test the scroll functionality here
+  it('scrolls to the top of the page', () => {
+    const wrapper = shallow(<BackToTopLink/>);
+    const wrapperInstance = wrapper.instance();
+    let handleClickStub = stub(wrapperInstance, '_scrollToTop');
+
+    wrapperInstance.forceUpdate();
+    wrapper.update();
+
+    wrapper.find('#back-to-top-link').simulate('click');
+
+    expect(handleClickStub).to.have.been.called;
+  });
 });
