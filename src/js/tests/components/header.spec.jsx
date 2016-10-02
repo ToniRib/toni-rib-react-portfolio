a5ff44ed1fdfa0f0b8a6620647bbc7483fb52d4e
ToniRib <tonimarierib@gmail.com>
2016-10-01 21:00:15 -0600

    Test that Header renders the MainImage component

--- src/js/tests/components/header.spec.jsx
+++ src/js/tests/components/header.spec.jsx
@@ -1,9 +1,16 @@
 import Header from '../../components/header';
+import MainImage from '../../components/main_image';
 
 describe('<Header/>', () => {
   it('should render a header', () => {
     const wrapper = shallow(<Header/>);
 
     expect(wrapper).to.have.tagName('header');
+  });
+
+  it('should render the MainImage component', () => {
+    const wrapper = shallow(<Header/>);
+
+    expect(wrapper).to.contain(<MainImage/>)
   });
 });
