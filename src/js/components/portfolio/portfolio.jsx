c1ef4fe5bdccadb90218236389aef0a13280c873
ToniRib <tonimarierib@gmail.com>
2016-10-10 16:21:34 -0600

    Add the Portfolio component

---
+++ src/js/components/portfolio/portfolio.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import BackToTopLink from '../common/back_to_top_link';
+import SectionTitle from '../common/section_title';
+import Project from './project';
+
+class Portfolio extends React.Component {
+  render () {
+    return (
+      <article className='row'>
+        <SectionTitle name='Portfolio' id='portfolio'/>
+        <div className='row portfolio-row'>
+          <Project id='runhacker' name='runHacker' image='./img/run-hacker.svg' alt='Column Chart' />
+          <Project id='freelancer' name='Freelancer' image='./img/freelancer.svg' alt='Wrench' />
+          <Project id='sha1' name='SHA-1 Algorithm' image='./img/sha1-algorithm.svg' alt='Pound Sign' />
+        </div>
+        <BackToTopLink />
+      </article>
+    );
+  }
+}
+
+module.exports = Portfolio;
---
+++ src/js/tests/components/portfolio/portfolio.spec.jsx
@@ -0,0 +1,23 @@
+import Portfolio from '../../../components/portfolio/portfolio';
+import SectionTitle from '../../../components/common/section_title';
+import BackToTopLink from '../../../components/common/back_to_top_link';
+
+describe('<Portfolio />', () => {
+  it('renders an article', () => {
+    const wrapper = shallow(<Portfolio />);
+
+    expect(wrapper).to.have.tagName('article');
+  });
+
+  it('includes a section title with the name Portfolio', () => {
+    const wrapper = shallow(<Portfolio />);
+
+    expect(wrapper).to.contain(<SectionTitle name='Portfolio' id='portfolio' />)
+  });
+
+  it('includes a link back to the top', () => {
+    const wrapper = shallow(<Portfolio />);
+
+    expect(wrapper).to.contain(<BackToTopLink />)
+  });
+});
