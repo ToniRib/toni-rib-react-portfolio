74c6761aebf2d1683076c300254dafb3ca2b5174
ToniRib <tonimarierib@gmail.com>
2016-10-10 17:01:36 -0600

    Add a list of the contact links

---
+++ src/js/contact_links.js
@@ -0,0 +1,28 @@
+const contactLinks = [
+  {
+    id: 1,
+    name: 'ToniRib',
+    type: 'linkedin',
+    link: 'http://linkedin.com/in/ToniRib'
+  },
+  {
+    id: 2,
+    name: 'tonirib',
+    type: 'github',
+    link: 'http://github.com/tonirib'
+  },
+  {
+    id: 3,
+    name: 'tonimarierib',
+    type: 'twitter',
+    link: 'http://twitter.com/tonimarierib'
+  },
+  {
+    id: 4,
+    name: 'tonimarierib@gmail.com',
+    type: 'gmail',
+    link: 'mailto:tonimarierib@gmail.com'
+  }
+];
+
+module.exports = contactLinks;
